a08291190fd1d0ab3f2b7311affc8cb6ba32d20a
Devin Chen <taunwe79@gmail.com>
2022-07-20 09:07:58 -0500

    created mysql database for heroes_vs_villains and created the supers and super_types app files for the project

---
+++ supers/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Super
+
+class SuperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Super
+        fields = ['id','name','alter_ego','catchphrase','super_type','super_type_id']
+        depth = 1
+    super_type_id = serializers.IntegerField(write_only = True)
